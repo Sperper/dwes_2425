9a96e8db070ab9af4db82de901c758995d922dca
unknown <sperper2907@g.educaand.es>
2024-11-14 12:33:07 +0100

    Libro

---
+++ tema04/proyecto/03-libros-se/mostrar.php
@@ -0,0 +1,13 @@
+<?php
+
+# Clases
+include 'class/class.libro.php';
+include 'class/class.tabla_libros.php';
+
+# Librerias
+
+# Model
+include 'models/model.mostrar.php';
+
+# Vista
+include 'views/view.mostrar.php';
